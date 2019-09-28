df37ff0edb2f3e5070ef26db0e57abee41eee2ba
kesihinz <keishin.chou@gmail.com>
2019-09-28 10:19:47 +0900

    Add Firebase settings and Register functions.

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
+import Firebase
 
-
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
 
     
     //Pre-linked IBOutlets
@@ -19,25 +19,40 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailTextfield.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
+    func textFieldShouldReturn(_ emailTextfield: UITextField) -> Bool {
+       // Try to find next responder
+       if let nextField = emailTextfield.superview?.viewWithTag(emailTextfield.tag + 1) {
+          nextField.becomeFirstResponder()
+       } else {
+          // Not found, so remove keyboard.
+          emailTextfield.resignFirstResponder()
+       }
+       // Do not add a line break
+       return true
+    }
   
     @IBAction func registerPressed(_ sender: AnyObject) {
+                
+        //TODO: Set up a new user on our Firbase database. Need Optional Bindings!
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Registration Successful.")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
-
-        
-        //TODO: Set up a new user on our Firbase database
-        
-        
-
-        
-        
-    } 
+    }
     
     
 }
