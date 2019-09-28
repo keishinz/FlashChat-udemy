5301ec0043764e6128d8f2111ce76a5bbde8cb71
kesihinz <keishin.chou@gmail.com>
2019-09-28 22:39:37 +0900

    Add some basic functions in ChatView and LoginView.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
     // Declare instance variables here
 
@@ -26,42 +27,65 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
+        messageTableView.dataSource = self
+        messageTableView.delegate = self
         
         
         //TODO: Set yourself as the delegate of the text field here:
-
+        messageTextfield.delegate = self
         
         
         //TODO: Set the tapGesture here:
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
         
 
         //TODO: Register your MessageCell.xib file here:
-
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
+        //Call function to configure the TableView
+        configureTableView()
     }
 
     ///////////////////////////////////////////
     
     //MARK: - TableView DataSource Methods
-    
+
     
     
     //TODO: Declare cellForRowAtIndexPath here:
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        let messageArray = ["first message", "second message", "third message"]
+        cell.messageBody.text = messageArray[indexPath.row]
+        
+        return cell
+    }
     
     
     //TODO: Declare numberOfRowsInSection here:
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 2
+//    } numberOfSections function
     
     
     //TODO: Declare tableViewTapped here:
-    
+    @objc func tableViewTapped(_ tapGesture: UITapGestureRecognizer) {
+        //textFieldDidEndEditing(messageTextfield)
+        messageTextfield.endEditing(true)
+    }
     
     
     //TODO: Declare configureTableView here:
-    
+    func configureTableView() {
+        messageTableView.rowHeight = UITableView.automaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
     
     
     ///////////////////////////////////////////
@@ -72,12 +96,23 @@ class ChatViewController: UIViewController {
 
     
     //TODO: Declare textFieldDidBeginEditing here:
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 358
+            self.view.layoutIfNeeded()
+        }
+    }
     
     
     
     //TODO: Declare textFieldDidEndEditing here:
-    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
 
     
     ///////////////////////////////////////////
@@ -107,7 +142,12 @@ class ChatViewController: UIViewController {
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
         //TODO: Log out the user and send them back to WelcomeViewController
-        
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch {
+            print("error, there was a network problem.")
+        }
         
     }
     
--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Firebase
 
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, UITextFieldDelegate {
 
     //Textfields pre-linked with IBOutlets
     @IBOutlet var emailTextfield: UITextField!
@@ -18,16 +18,30 @@ class LogInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        emailTextfield.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    func textFieldShouldReturn(_ emailTextfield: UITextField) -> Bool {
+       // Try to find next responder
+       if let nextField = emailTextfield.superview?.viewWithTag(emailTextfield.tag + 1) {
+          nextField.becomeFirstResponder()
+       } else {
+          // Not found, so remove keyboard.
+          emailTextfield.resignFirstResponder()
+       }
+       // Do not add a line break
+       return true
+    }
+
    
     @IBAction func logInPressed(_ sender: AnyObject) {
         
-        //TODO: Log in the user
+        //TODO: Log in the user. Need optional bindings.
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
             (user, error) in
             if error != nil {
@@ -36,7 +50,6 @@ class LogInViewController: UIViewController {
                 print("Login Successful.")
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
-          // ...
         }
         
         
